202d7c7738f9f168e5e19986d6e78432c704fa79
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-07-25 16:15:45 +0100

    added pure js impl

---
+++ script.js
@@ -0,0 +1,57 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter-button');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+async function getQuoteFromApi() {
+  showLoadingSpinner();
+  // We need to use a Proxy URL to make our API call in order to avoid a CORS error
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
+    removeLoadingSpinner();
+  } catch (error) {
+    getQuote();
+  }
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', getQuoteFromApi);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuoteFromApi();
